11e95d6cba1d54f2558f8bdcc31fb3aa07d1ca99
Vitaly Kuzyaev <37330183+vitkuz573@users.noreply.github.com>
2025-07-05 20:33:42 +0500

    Add validation for ParameterResource

--- src/Aspirate.Processors/Resources/AbstractProcessors/ParameterProcessor.cs
+++ src/Aspirate.Processors/Resources/AbstractProcessors/ParameterProcessor.cs
@@ -8,8 +8,35 @@ public class ParameterProcessor(IFileSystem fileSystem, IAnsiConsole console,
     public override string ResourceType => AspireComponentLiterals.Parameter;
 
     /// <inheritdoc />
-    public override Resource? Deserialize(ref Utf8JsonReader reader) =>
-        JsonSerializer.Deserialize<ParameterResource>(ref reader);
+    public override Resource? Deserialize(ref Utf8JsonReader reader)
+    {
+        var parameter = JsonSerializer.Deserialize<ParameterResource>(ref reader);
+
+        ValidateParameterResource(parameter);
+
+        return parameter;
+    }
+
+    private static void ValidateParameterResource(ParameterResource? parameter)
+    {
+        if (parameter == null)
+        {
+            throw new InvalidOperationException(
+                $"{AspireComponentLiterals.Parameter} not found.");
+        }
+
+        if (parameter.Value is null)
+        {
+            throw new InvalidOperationException(
+                $"{AspireComponentLiterals.Parameter} {parameter.Name} missing required property 'value'.");
+        }
+
+        if (parameter.Inputs is null)
+        {
+            throw new InvalidOperationException(
+                $"{AspireComponentLiterals.Parameter} {parameter.Name} missing required property 'inputs'.");
+        }
+    }
 
     public override Task<bool> CreateManifests(CreateManifestsOptions options) =>
         // Do nothing for Parameter Resources, they are there for configuration.
