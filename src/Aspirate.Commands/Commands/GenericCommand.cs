bb6b40cb34b0cd317ae8fce2471165667ec7a385
Vitaly Kuzyaev <37330183+vitkuz573@users.noreply.github.com>
2025-07-11 14:06:18 +0500

    fix(settings): initialize services for generic commands

--- src/Aspirate.Commands/Commands/GenericCommand.cs
+++ src/Aspirate.Commands/Commands/GenericCommand.cs
@@ -5,7 +5,23 @@ public class GenericCommand : Command
 {
     public GenericCommand(string name, string description)
         : base(name, description) =>
-        Action = CommandHandler.Create<IServiceCollection>(ExecuteCommand);
+        Action = CommandHandler.Create(InvokeCommand);
+
+    private Task<int> InvokeCommand()
+    {
+        var services = new ServiceCollection();
+        services
+            .AddSecretProtectionStrategies()
+            .AddAspirateState()
+            .AddAspirateServices()
+            .AddAspirateActions()
+            .AddAspirateProcessors()
+            .AddAspirateSecretProvider()
+            .AddPlaceholderTransformation()
+            .AddSingleton(AnsiConsole.Console);
+
+        return ExecuteCommand(services);
+    }
 
     protected virtual Task<int> ExecuteCommand(IServiceCollection services) => Task.FromResult(0);
 
