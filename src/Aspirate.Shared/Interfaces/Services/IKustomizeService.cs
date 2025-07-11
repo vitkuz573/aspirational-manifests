10685bbcdc3049fa6ba98295b9cd38d00687788e
Vitaly Kuzyaev <vitkuz573@gmail.com>
2025-07-11 15:33:31 +0500

    UPD

--- src/Aspirate.Shared/Interfaces/Services/IKustomizeService.cs
+++ src/Aspirate.Shared/Interfaces/Services/IKustomizeService.cs
@@ -2,7 +2,7 @@ namespace Aspirate.Shared.Interfaces.Services;
 
 public interface IKustomizeService
 {
-    Task<string> RenderManifestUsingKustomize(string kustomizePath);
+    Task<string> RenderManifestUsingKustomize(string kustomizePath, string? overlayPath = null);
     Task WriteSecretsOutToTempFiles(AspirateState state, List<string> files, ISecretProvider secretProvider);
     Task<string?> WriteImagePullSecretToTempFile(AspirateState state, ISecretProvider provider);
     void CleanupSecretEnvFiles(bool? disableSecrets, IEnumerable<string> secretFiles);
